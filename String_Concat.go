a8ecab6e2bc7fc6f231ac386ae550bcceae04df7
Onkar Kulkarni <onkarkulkarni5404@gmail.com>
2020-05-18 09:03:16 +0530

    Simple String Concat

---
+++ String_Concat.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main(){
+    var string1 string = "onkar is a very good boy"
+    var string2 string = " and a enthusiast coder too"
+    var addthem=string1+string2
+    fmt.Println(addthem)
+
+}
